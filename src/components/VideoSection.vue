8ae873f781a3ac5dc8190f4150c256a14aa81c7c
monstar911 <122865320+monstar911@users.noreply.github.com>
2025-02-20 06:59:24 +0900

    fix: convert traditional css to tailwindcss for VideoSection.vue

--- src/components/VideoSection.vue
+++ src/components/VideoSection.vue
@@ -1,148 +1,9 @@
-<!-- <template>
-  <section class="video-section">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <div
-          class="wistia_responsive_padding"
-          style="padding: 56.25% 0 0 0; position: relative"
-        >
-          <div
-            class="wistia_responsive_wrapper"
-            style="
-              height: 100%;
-              left: 0;
-              position: absolute;
-              top: 0;
-              width: 100%;
-            "
-          >
-            <div
-              class="wistia_video_foam_dummy"
-              data-source-container-id=""
-              style="
-                border: 0px;
-                display: block;
-                height: 0px;
-                margin: 0px;
-                padding: 0px;
-                position: static;
-                visibility: hidden;
-                width: auto;
-              "
-            ></div>
-            <iframe
-              src="https://fast.wistia.net/embed/iframe/idwknzjwun?seo=false&amp;videoFoam=true&amp;resumeable=true"
-              title=""
-              allow="autoplay; fullscreen"
-              allowtransparency="true"
-              frameborder="0"
-              scrolling="no"
-              class="wistia_embed"
-              name="wistia_embed"
-              msallowfullscreen=""
-              width="1071"
-              height="602"
-              style="width: 1071px; height: 602px"
-            ></iframe>
-          </div>
-        </div>
-
-        <script>
-          var handleWistia = function () {
-            if (!document.querySelector("#wistia-ev")) {
-              var el = document.createElement("script");
-              el.setAttribute("id", "wistia-ev");
-              el.setAttribute("type", "text/javascript");
-              el.setAttribute(
-                "src",
-                "//fast.wistia.com/assets/external/E-v1.js"
-              );
-              document.body.append(el);
-            }
-            if (!document.querySelector("#wistia-playback")) {
-              var el = document.createElement("script");
-              el.setAttribute("id", "wistia-playback");
-              el.setAttribute("type", "text/javascript");
-              el.setAttribute(
-                "src",
-                "//dlbf54swf3l6o.cloudfront.net/scripts/wistia-playback-control.js"
-              );
-              document.body.append(el);
-            }
-          };
-          window.addEventListener("load", handleWistia);
-        </script>
-      </div>
-    </div> -->
-
-<!-- <div class="video-container">
-      <video
-        data-v-850c51fb=""
-        id="wistia_simple_video_92"
-        crossorigin="anonymous"
-        poster="https://fast.wistia.net/assets/images/blank.gif"
-        aria-label="Video"
-        src="https://fast.wistia.net/a187f492-f69e-43c3-abea-33c47fe82cef"
-        controls=""
-        controlslist="nodownload"
-        playsinline=""
-        preload="auto"
-        type="video/m3u8"
-        x-webkit-airplay="allow"
-        style="
-          background: transparent;
-          display: block;
-          height: 100%;
-          max-height: none;
-          max-width: none;
-          position: static;
-          visibility: visible;
-          width: 100%;
-          object-fit: contain;
-        "
-      ></video>
-    </div> -->
-<!-- </section> -->
-<!-- </template> -->
-
-<!-- <style scoped>
-.video-section {
-  max-width: 800px;
-  margin: 2rem auto;
-}
-
-.video-container {
-  position: relative;
-  padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
-  height: 0;
-  overflow: hidden;
-  /* border-radius: 8px; */
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.video-placeholder {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.3);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.5rem;
-  color: white;
-}
-</style> -->
 <template>
-  <section class="video-section">
-    <div class="container">
-      <div class="wistia_responsive_padding">
-        <div class="wistia_responsive_wrapper">
-          <div
-            class="wistia_video_foam_dummy"
-            data-source-container-id=""
-          ></div>
+  <section class="w-full my-8">
+    <div class="max-w-[var(--container-width)] mx-auto px-4 w-full">
+      <div class="relative pt-[56.25%]">
+        <div class="absolute top-0 left-0 h-full w-full">
+          <div class="hidden" data-source-container-id=""></div>
           <iframe
             src="https://fast.wistia.net/embed/iframe/idwknzjwun?seo=false&videoFoam=true&resumeable=true"
             title="Wistia video player"
@@ -150,7 +11,7 @@
             allowtransparency="true"
             frameborder="0"
             scrolling="no"
-            class="wistia_embed"
+            class="absolute top-0 left-0 w-full h-full border-0"
             name="wistia_embed"
             msallowfullscreen
           ></iframe>
@@ -201,43 +62,3 @@ const loadWistiaScripts = async () => {
 
 onMounted(loadWistiaScripts);
 </script>
-
-<style scoped>
-.video-section {
-  width: 100%;
-  margin: 2rem auto;
-}
-
-.container {
-  max-width: var(--container-width);
-  margin: 0 auto;
-  padding: 0 1rem 0 1rem;
-  width: 100%;
-}
-
-.wistia_responsive_padding {
-  padding-top: 56.25%; /* 16:9 Aspect Ratio */
-  position: relative;
-}
-
-.wistia_responsive_wrapper {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-}
-
-.wistia_video_foam_dummy {
-  display: none;
-}
-
-.wistia_embed {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: 0;
-}
-</style>
