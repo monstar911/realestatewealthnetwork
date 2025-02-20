30275a1d46e522c63e33796e1647a7ad33392352
monstar911 <122865320+monstar911@users.noreply.github.com>
2025-02-21 01:40:06 +0900

    fix: footer

--- src/components/Section-99-Footer.vue
+++ src/components/Section-99-Footer.vue
@@ -1,14 +1,11 @@
 <template>
-  <section>
-    <!-- Footer -->
-    <footer
-      class="mt-16 text-center text-white/80 bg-[#474747] block w-full max-w-[300px] h-auto mx-auto mb-4 border-0"
-    >
+  <!-- Footer -->
+  <footer class="bg-[#474747] w-full h-auto mx-auto">
+    <section class="section-image m-6">
       <img
         src="https://dlbf54swf3l6o.cloudfront.net/images/logos/rewn-logo-light.png"
         alt="Real Estate Wealth Network"
-        class="max-w-[200px] mb-4"
       />
-    </footer>
-  </section>
+    </section>
+  </footer>
 </template>
